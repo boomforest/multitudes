1fda0cfddeaa0261b43236d23e9d2d403494d98b
boomforest <31712853+boomforest@users.noreply.github.com>
2025-07-09 18:43:00 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+function App() {
+  return <h1 style={{padding: '20px'}}>Hello World Test!</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
