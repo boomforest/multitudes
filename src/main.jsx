952c2f55ea8e92bfa8ddb5a1d0d74d9475dcb85c
boomforest <31712853+boomforest@users.noreply.github.com>
2025-07-10 05:57:47 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
+import MetaMaskWallet from './MetaMaskWallet';
 
 function App() {
   const [supabase, setSupabase] = useState(null)
